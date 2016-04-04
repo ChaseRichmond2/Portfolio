346e292f73f7c8de3f1cfac2ad2dc02b3d2db565
ChaseRichmond2 <pdt.crichmond@gmail.com>
2016-04-04 18:16:45 -0400

    Finalized the Gallery section

--- main.py
+++ main.py
@@ -22,13 +22,20 @@ class MainHandler(webapp2.RequestHandler):
                 self.response.write(template.render({'title':'Experience'}))
             elif path == '/gallery.html':
                 self.response.write(template.render({'title':'Gallery'}))
-            elif path == 'contact.html':
-                self.response.write(template.render({'title':'Contact'}))
             else:
                 self.response.write(template.render({'title':'Index'}))
         except:
             template = JINJA_ENVIRONMENT.get_template('templates/index.html')
             self.response.write(template.render({'title':'Index'}))
+class ContactHandler(webapp2.RequestHandler):
+    def get(self):
+        logging.info("Contact")
+        template = JINJA_ENVIRONMENT.get_template('templates/contact.html')
+        self.response.write(template.render({'title':'Homepage','Home':'HOME', 'Family':'Family','School':'School', 'Login': 'Login'})) 
+    def post(self):
+        logging.info("POST for Login Handler")
+        name = self.request.get("name") #These read everything in as a string so we must use ""
+        email = self.request.get("email")
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/index.html', MainHandler),
